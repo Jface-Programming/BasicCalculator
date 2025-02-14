535ad21aca496294906fe415e41b181048610929
Jface-Programming <jarvisti76@googlemail.com>
2025-02-15 09:55:45 +1300

    BasicCalculator

--- main.cpp
+++ main.cpp
@@ -5,30 +5,33 @@ int main() {
     double num1;
     double num2;
     char op;
-    cout << "enter the first number: ";
-    cin >> num1;
-    cout << endl;
-    cout << "enter the second number: ";
-    cin >> num2;
-    cout << endl;
-    cout << "enter the operation + * - /: ";
-    cin >> op;
-    cout << endl;
-    switch (op) {
-        case '+':
-            cout << num1 + num2 << endl;
-        break;
-        case '-':
-            cout << num1 - num2 << endl;
-        break;
-        case '*':
-            cout << num1 * num2 << endl;
-        break;
-        case '/':
-            cout << num1 / num2 << endl;
-        break;
-        default:
-            cout << "please enter a valid operation." << endl;
+    bool loop = true;
+    while (loop == true) {
+        cout << "enter the first number: ";
+        cin >> num1;
+        cout << endl;
+        cout << "enter the second number: ";
+        cin >> num2;
+        cout << endl;
+        cout << "enter the operation + * - /: ";
+        cin >> op;
+        cout << endl;
+        switch (op) {
+            case '+':
+                cout << num1 + num2 << endl;
+            break;
+            case '-':
+                cout << num1 - num2 << endl;
+            break;
+            case '*':
+                cout << num1 * num2 << endl;
+            break;
+            case '/':
+                cout << num1 / num2 << endl;
+            break;
+            default:
+                cout << "please enter a valid operation." << endl;
+        }
     }
     return 0;
 }
